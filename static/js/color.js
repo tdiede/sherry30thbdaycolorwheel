acc05a5f2660463384fa76474da420f2d42094d1
tdiede <tdiede@gmail.com>
2016-11-30 09:13:33 -0800

    Editing js.

--- static/js/color.js
+++ static/js/color.js
@@ -3,7 +3,10 @@
 
 // set canvas and ctx
 var canvas = document.getElementById('canvas-picker');
-var ctx = canvas.getContext('2d');
+var ctx;
+canvas.onload = function() {
+    ctx = canvas.getContext('2d');
+};
 // create an image object
 var img = new Image();
 img.onload = addImageToCanvas;
