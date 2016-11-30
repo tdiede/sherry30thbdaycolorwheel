4ca6bf13aec4ccd8ccb1a87827c8bea843ee363f
tdiede <tdiede@gmail.com>
2016-11-30 09:43:47 -0800

    Added images back.

--- static/js/color.js
+++ static/js/color.js
@@ -4,6 +4,7 @@
 // set canvas and ctx
 var canvas = document.getElementById('canvas-picker');
 var ctx = canvas.getContext('2d');
+canvas.width = canvas.height = 0;
 // create an image object
 var img = new Image();
 img.onload = addImageToCanvas;
